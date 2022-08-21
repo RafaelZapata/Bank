b4792f65dac96eb9d04d8e48a6c77e9e889e6c1f
RafaelZapata <faelzapata2011@gmail.com>
2022-08-21 08:23:48 -0300

    Adding login page

--- server/src/controllers/Banker/BankerClientController.js
+++ server/src/controllers/Banker/BankerClientController.js
@@ -6,46 +6,49 @@ const COLLECTION = "users";
 const TYPE = "client";
 
 export default class BankerClientController {
-  // GET /banker/clients[?=id]
-  static async list(req, res) {
-    try {
-      const { id } = (req.query || {});
-      const collection = await req.database.collection(COLLECTION);
-      let data = [];
+    // GET /banker/clients[?=id]
+    static async list(req, res) {
+        try {
+            const { id } = req.query || {};
+            const collection = await req.database.collection(COLLECTION);
+            let data = [];
 
-      if (typeof id === "undefined") {
-        data = await collection.find({ role: TYPE }).sort({ date: -1 }).toArray()
-        data = data.map(item => new User({ ...item, password: undefined }));
-      } else {
-        data = await collection.findOne({ role: TYPE, _id: ObjectId(id) });
-        data = new User({ ...data, password: undefined });
-      }
+            if (typeof id === "undefined") {
+                data = await collection.find({ role: TYPE }).sort({ date: -1 }).toArray();
+                data = data.map((item) => new User({ ...item, password: undefined }));
+            } else {
+                data = await collection.findOne({ role: TYPE, _id: ObjectId(id) });
+                data = new User({ ...data, password: undefined });
+            }
 
-      return res.json({ ...Codes.get(200), data });
-    } catch (e) {
-      console.log(e);
-      return res.status(500).json(Codes.get(500));
+            return res.json({ ...Codes.get(200), data });
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json(Codes.get(500));
+        }
     }
-  }
 
-  // PATCH /banker/client/:id
-  static async update(req, res) {
-    try {
-      const collection = await req.database.collection(COLLECTION);
-      const clientID = ObjectId(req.params.id);
-      const client = new User(req.body, false);
+    // PATCH /banker/client/:id
+    static async update(req, res) {
+        try {
+            const collection = await req.database.collection(COLLECTION);
+            const clientID = ObjectId(req.params.id);
+            const client = new User(req.body, false);
 
-      const data = await collection.updateOne({ _id: clientID }, {
-        $set: client
-      });
+            const data = await collection.updateOne(
+                { _id: clientID },
+                {
+                    $set: client,
+                }
+            );
 
-      if (typeof data.modifiedCount === "undefined")
-        return res.status(500).json(Codes.get(500));
+            if (typeof data.modifiedCount === "undefined")
+                return res.status(500).json(Codes.get(500));
 
-      return res.json( Codes.get(200) );
-    } catch (e) {
-      console.log(e);
-      return res.status(500).json(Codes.get(500));
+            return res.json(Codes.get(200));
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json(Codes.get(500));
+        }
     }
-  }
 }
